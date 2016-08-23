f2bb64edfe2766750c661406eba2464d2c4704e2
Matt Waggoner <matt@mattwaggs.com>
2016-08-22 23:28:36 -0600

    added install script for my custom ubuntu themes

---
+++ .setup-files/install-ubuntu-themes.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/mattwaggs/ubuntu-royal-white
+mv ubuntu-royal-white /usr/share/themes/
+
+git clone https://github.com/mattwaggs/ultra-mono-light-combo-icons
+mv ultra-mono-light-combo-icons /usr/share/icons/
+
+echo 'Do you have unity-tweak-tool yet?'
