27c41da1074cfe61b1846a3588edf60d9aecd2e1
Matt <matt@mattwaggs.com>
2020-03-18 00:29:50 -0600

    remove old files

--- .setup-files/install-powerline-fonts.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo 'you may need sudo.'
-git clone https://github.com/powerline/fonts
-./fonts/install.sh
-echo 'removing temp fonts folder.'
-rm -r fonts
-echo 'done.' 
--- .setup-files/install-ubuntu-themes.sh
+++
@@ -1,7 +0,0 @@
-git clone https://github.com/mattwaggs/ubuntu-royal-white
-mv ubuntu-royal-white /usr/share/themes/
-
-git clone https://github.com/mattwaggs/ultra-mono-light-combo-icons
-mv ultra-mono-light-combo-icons /usr/share/icons/
-
-echo 'Do you have unity-tweak-tool yet?'
